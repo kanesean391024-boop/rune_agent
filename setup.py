4968152affa9e935de2e5b76634a8c300053434b
Sean Kane <kanesean391024@gmail.com>
2025-10-06 09:57:09 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="rune_agent",
+    version="0.1.0",
+    packages=find_packages(),
+    entry_points={"console_scripts": ["rune-agent = rune_agent:main"]},  # Assumes a main() in your script
+    description="Elder Futhark Rune Transliterater",
+    author="You",
+)
